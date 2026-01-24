f8e8f6fd49f54438fb7039a910ca62e06a8b6782
Gabrielbemo <gabrielbemoura@gmail.com>
2026-01-24 18:25:11 -0300

    adding notification and kafka  configuration

--- services/payment/src/main/java/com/gabriel/ecommerce/payment/PaymentService.java
+++ services/payment/src/main/java/com/gabriel/ecommerce/payment/PaymentService.java
@@ -1,5 +1,7 @@
 package com.gabriel.ecommerce.payment;
 
+import com.gabriel.ecommerce.notification.NotificationProducer;
+import com.gabriel.ecommerce.notification.PaymentNotificationRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,9 +14,18 @@ public class PaymentService {
 
     private final PaymentRepository paymentRepository;
     private final PaymentMapper paymentMapper;
+    private final NotificationProducer notificationProducer;
 
     public UUID createPayment(@Valid PaymentRequest request) {
         var payment = paymentRepository.save(paymentMapper.toPayment(request));
-        return null;
+        notificationProducer.sendNotification(new PaymentNotificationRequest(
+                request.orderReference(),
+                request.amount(),
+                request.paymentMethod(),
+                request.customer().firstName(),
+                request.customer().lastName(),
+                request.customer().email()
+        ));
+        return payment.getId();
     }
 }
