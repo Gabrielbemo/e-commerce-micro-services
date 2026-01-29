5eaca91003dd3f23461dd20cf73c10a719c46648
Gabrielbemo <gabrielbemoura@gmail.com>
2026-01-28 22:04:22 -0300

    fixing gateway

--- services/gateway-server/src/main/java/com/gabriel/gatewayserver/GatewayServerApplication.java
+++ services/gateway-server/src/main/java/com/gabriel/gatewayserver/GatewayServerApplication.java
@@ -1,13 +1,13 @@
-package com.gabriel.gateway;
+package com.gabriel.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GatewayApplication {
+public class GatewayServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
+		SpringApplication.run(GatewayServerApplication.class, args);
 	}
 
 }
